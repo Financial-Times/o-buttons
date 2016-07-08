def326149e28d0bf384bf25c5706295fd7db3c44
Benavente Gomez <maria.benaventegomez@ft-mw4343.ftroot.com>
2016-07-08 17:33:02 +0100

    package.json and pa11yDemo.js added

---
+++ pa11yDemo.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const pa11y = require('pa11y');
+const cliReporter = require('pa11y/reporter/cli');
+
+const test = pa11y({
+  log: {
+    debug: console.log.bind(console),
+    error: console.error.bind(console),
+    info: console.info.bind(console)
+  }
+});
+const cwd = process.cwd();
+const demoPath = `file:${cwd}/demos/local/test.html`;
+test.run(demoPath, (error, result) => {
+  if(error){
+    return console.log(`Ooops:${error.message}`);
+  }
+  cliReporter.results(result, demoPath);
+});
