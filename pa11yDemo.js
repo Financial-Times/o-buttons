80948bf4a6918703c35cb09bf4702c7ba9c6ea99
Benavente Gomez <maria.benaventegomez@ft-mw4343.ftroot.com>
2016-07-08 17:51:53 +0100

    pa11yDemo bug (hopefully) fixed

--- pa11yDemo.js
+++ pa11yDemo.js
@@ -1,5 +1,4 @@
-'use strict';
-
+/*global process*/
 const pa11y = require('pa11y');
 const cliReporter = require('pa11y/reporter/cli');
 
@@ -11,7 +10,7 @@ const test = pa11y({
   }
 });
 const cwd = process.cwd();
-const demoPath = `file:${cwd}/demos/local/test.html`;
+const demoPath = `file://${cwd}/demos/local/test.html`;
 test.run(demoPath, (error, result) => {
   if(error){
     return console.log(`Ooops:${error.message}`);
