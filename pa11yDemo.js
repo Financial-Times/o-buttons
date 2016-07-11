a6f937fe06ac842dc27c22f5339d560b1541616b
Benavente Gomez <maria.benaventegomez@ft-mw4343.ftroot.com>
2016-07-11 11:30:31 +0100

    link anchor href-void exception added

--- pa11yDemo.js
+++ pa11yDemo.js
@@ -7,10 +7,11 @@ const test = pa11y({
     debug: console.log.bind(console),
     error: console.error.bind(console),
     info: console.info.bind(console)
-  }
-});
+  },
+  ignore: ["WCAG2AA.Principle2.Guideline2_4.2_4_1.G1,G123,G124.NoSuchID"]
+}); 
 const cwd = process.cwd();
-const demoPath = `file://${cwd}/demos/local/test.html`;
+const demoPath = `file://${cwd}/demos/local/pa11y.html`;
 test.run(demoPath, (error, result) => {
   if(error){
     return console.log(`Ooops:${error.message}`);
